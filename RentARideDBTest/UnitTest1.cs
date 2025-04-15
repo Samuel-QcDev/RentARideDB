1890be4d6310cd81c4876b796690c150080b8d6a
Samuel Lassonde <slassonde@gmail.com>
2025-04-15 11:10:03 -0400

    Add the Unit test to Git Changes

---
+++ RentARideDBTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+
+namespace RentARideDBTest
+{
+    public class PreferenceTests
+    {
+        [Fact]
+        public void MustBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
